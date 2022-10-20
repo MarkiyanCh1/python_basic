1775577e0e750aa46067f1615cbc10155db7117d
Mark <markiyan.chornovil@gmail.com>
2022-10-20 21:38:48 +0300

    data and types of data

---
+++ data_types.py
@@ -0,0 +1,21 @@
+user_name = "John"
+user_surname = "Doe"
+user_age = 20
+copyright_statement = "Free copyright"
+number_of_copies = 23
+validation_question = "What is your age?"
+current_month = "October"
+days_in_month = 31
+process_status = "Not finished"
+number_of_files_processed = 12
+
+print(user_name, ",", type(user_name))
+print(user_surname, ",", type(user_surname))
+print(user_age, ",", type(user_age))
+print(copyright_statement, ",", type(copyright_statement))
+print(number_of_copies, ",", type(number_of_copies))
+print(validation_question, ",", type(validation_question))
+print(current_month, ",", type(current_month))
+print(days_in_month, ",", type(days_in_month))
+print(process_status, ",", type(process_status))
+print(number_of_files_processed, ",", type(number_of_files_processed))
