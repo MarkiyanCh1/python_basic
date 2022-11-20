0bf33356a65b741080d9b4c0a789685b4451274d
Mark <markiyan.chornovil@gmail.com>
2022-11-20 14:55:42 +0200

    loops

--- loops.py
+++ loops.py
@@ -1,29 +1,29 @@
-# # 1
-# counter = 0
-# element = " "
-# row_numbers = 3
-# vowels = "AaEeIiOoUuYy"
-# while counter < 1:
-#     string = input('Enter any string: ')
-#     check_for_numbers = [number.isdigit() for number in string]
-#     number = 0
-#     for flag in check_for_numbers:
-#         if flag:
-#             number += 1
-#         else:
-#             number = 0
-#         if number == row_numbers:
-#             print("3 digits in a row were entered. The loop is interrupted")
-#             counter += 1
-#             break
-#     else:
-#         upper = (''.join(letter for letter in string if letter.isupper()))
-#         print('Uppercase characters:', upper)
-#         print(f"Index of spaces: {[space for space, character in enumerate(string) if character == element]}")
-#         vowel = [vow for vow in string if vow in vowels]
-#         print(f"Vowel letters: {vowel}")
-#         print("The loop has been completed successfully")
-#         counter += 1
+# 1
+counter = 0
+element = " "
+row_numbers = 3
+vowels = "AaEeIiOoUuYy"
+while counter < 1:
+    string = input('Enter any string: ')
+    check_for_numbers = [number.isdigit() for number in string]
+    number = 0
+    for flag in check_for_numbers:
+        if flag:
+            number += 1
+        else:
+            number = 0
+        if number == row_numbers:
+            print("3 digits in a row were entered. The loop is interrupted")
+            counter += 1
+            break
+    else:
+        upper = (''.join(letter for letter in string if letter.isupper()))
+        print('Uppercase characters:', upper)
+        print(f"Index of spaces: {[space for space, character in enumerate(string) if character == element]}")
+        vowel = [vow for vow in string if vow in vowels]
+        print(f"Vowel letters: {vowel}")
+        print("The loop has been completed successfully")
+        counter += 1
 
 # 2
 while True:
