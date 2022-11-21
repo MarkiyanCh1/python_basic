f6fbace5092dbf8883121ec63ebd086b064c6278
Mark <markiyan.chornovil@gmail.com>
2022-11-21 15:25:12 +0200

    loops

--- loops.py
+++ loops.py
@@ -20,47 +20,47 @@ for element in user_string:
 print('Uppercase characters:', ', '.join(upper))
 print(f"Index of spaces: {indexes_of_spaces}")
 print(f"Vowel letters: {vowels}")
-#
-# # 2
-# while True:
-#
-#     number_1 = str(input(f"Enter the first operand/Enter exit to complete: "))
-#     if number_1 == "exit":
-#         print("The process is completed")
-#         break
-#     number_2 = str(input(f"Enter the second operand/Enter exit to complete: : "))
-#     if number_2 == "exit":
-#         print("The process is completed")
-#         break
-#
-#     operation = str(input(f"Enter an operation/Enter exit to complete: "))
-#     if operation == "exit":
-#         print("The process is completed")
-#         break
-#
-#     if '.' in number_1:
-#         number_1 = float(number_1)
-#     else:
-#         number_1 = int(number_1)
-#
-#     if '.' in number_2:
-#         number_2 = float(number_2)
-#     else:
-#         number_2 = int(number_2)
-#
-#     print(number_1)
-#     print(number_2)
-#     if operation == '+':
-#         print(f'Result {number_1} {operation} {number_2} = {number_1 + number_2}')
-#     elif operation == '-':
-#         print(f'Result {number_1} {operation} {number_2} = {number_1 - number_2}')
-#     elif operation == '*':
-#         print(f'Result {number_1} {operation} {number_2} = {number_1 * number_2}')
-#     elif operation == '/' and number_2 == 0:
-#         print("Error - You can't divide on zero")
-#     elif operation == '/':
-#         print(f'Result {number_1} {operation} {number_2} = {number_1 / number_2}')
-#     elif operation == '**':
-#         print(f'Result {number_1} {operation} {number_2} = {number_1 ** number_2}')
-#     else:
-#         print("Error - Incorrect sign")
+
+# 2
+while True:
+
+    number_1 = str(input(f"Enter the first operand/Enter exit to complete: "))
+    if number_1 == "exit":
+        print("The process is completed")
+        break
+    number_2 = str(input(f"Enter the second operand/Enter exit to complete: : "))
+    if number_2 == "exit":
+        print("The process is completed")
+        break
+
+    operation = str(input(f"Enter an operation/Enter exit to complete: "))
+    if operation == "exit":
+        print("The process is completed")
+        break
+
+    if '.' in number_1:
+        number_1 = float(number_1)
+    else:
+        number_1 = int(number_1)
+
+    if '.' in number_2:
+        number_2 = float(number_2)
+    else:
+        number_2 = int(number_2)
+
+    print(number_1)
+    print(number_2)
+    if operation == '+':
+        print(f'Result {number_1} {operation} {number_2} = {number_1 + number_2}')
+    elif operation == '-':
+        print(f'Result {number_1} {operation} {number_2} = {number_1 - number_2}')
+    elif operation == '*':
+        print(f'Result {number_1} {operation} {number_2} = {number_1 * number_2}')
+    elif operation == '/' and number_2 == 0:
+        print("Error - You can't divide on zero")
+    elif operation == '/':
+        print(f'Result {number_1} {operation} {number_2} = {number_1 / number_2}')
+    elif operation == '**':
+        print(f'Result {number_1} {operation} {number_2} = {number_1 ** number_2}')
+    else:
+        print("Error - Incorrect sign")
