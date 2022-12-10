5b690b177c0f085e91d6e0d5dfeb3b4e76e8b355
Mark <markiyan.chornovil@gmail.com>
2022-12-10 20:43:36 +0200

    virtualenv_practice.py

---
+++ virtualenv_practice.py
@@ -0,0 +1,42 @@
+
+# 1. Create and activate a virtual environment
+# create -> C:\Users\User\PycharmProjects\envs>python -m venv new_venv
+# activate -> C:\Users\User\PycharmProjects\envs>new_venv\Scripts\activate.bat
+
+# 2. Install several modules of any kind
+# install django -> (new_venv) C:\Users\User\PycharmProjects\envs>pip install django
+# install kivy -> (new_venv) C:\Users\User\PycharmProjects\envs>pip install kivy
+# install pandas -> (new_venv) C:\Users\User\PycharmProjects\envs>pip install pandas
+# install requests -> (new_venv) C:\Users\User\PycharmProjects\envs>pip install requests
+# uninstall requests -> (new_venv) C:\Users\User\PycharmProjects\envs>pip uninstall requests
+
+# 2.2 Control
+# (new_venv) C:\Users\User\PycharmProjects\envs>pip freeze
+# asgiref==3.5.2
+# certifi==2022.12.7
+# charset-normalizer==2.1.1
+# Django==4.1.4
+# docutils==0.19
+# idna==3.4
+# Kivy==2.1.0
+# kivy-deps.angle==0.3.3
+# kivy-deps.glew==0.3.1
+# kivy-deps.sdl2==0.4.5
+# Kivy-Garden==0.1.5
+# numpy==1.23.5
+# pandas==1.5.2
+# Pygments==2.13.0
+# pypiwin32==223
+# python-dateutil==2.8.2
+# pytz==2022.6
+# pywin32==305
+# six==1.16.0
+# sqlparse==0.4.3
+# tzdata==2022.7
+# urllib3==1.26.13
+
+# 3. Save the list of project dependencies in requirements.txt
+# create requirements.txt -> (new_venv) C:\Users\User\PycharmProjects\envs>pip freeze > requirements.txt
+
+# 4. Deactivate the virtual environment
+# deactivate -> (new_venv) C:\Users\User\PycharmProjects\envs>deactivate
