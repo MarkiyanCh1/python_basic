af1548bd0b5164101a7a8ae248af523d3214e17d
Mark <markiyan.chornovil@gmail.com>
2023-01-20 15:39:22 +0200

    oop - two armies

---
+++ Two armies/unit_names.py
@@ -0,0 +1,10 @@
+warrior_names = ['Sir Lothain', 'Sir Ethal Great', 'Sir Xandyr', 'Sir Galahad', 'Sir Percynom',	'Sir Phocles',
+                 'Sir Linell',	'Sir Miskelen', 'Sir Trystran of Elysas', 'Sir Trtstran of Lyon', 'Sir Carethe',
+                 'Sir Rareth', 'Sir Gontas', 'Lady Solina', 'Sir Elubrys', 'Lady Brederis', 'Lady Lanconte',
+                 'Lady Circey', 'Merlin', 'Morgan le Fay', 'Elora', 'Tehimar', 'Hermione', 'Luna Moonglade',
+                 'Medea', 'Gendelf', 'Reberius', 'Melisia Delacour', 'Rakron Tiza', 'Rhunilorh Crelthendellal',
+                 'Thezahl Filch' 'Otorn Crabbe', 'Jessica Atréides', 'Anomander', 'Malazan', 'Jasnah Kholin',
+                 'Beric Dondarrion', 'Emhyr of Emreis', 'Celebrimbor', 'Malazan', 'Weatherwax', 'Abadda', 'Abarat',
+                 'Adamar', 'Adorellan', 'Adresin', 'Aduce', 'Aelrindel', 'Aerendyl', 'Aermhar', 'Aesar', 'Aeson',
+                 'Afamrail', 'Agis', 'Aglanthol', 'Aglanthol', 'Ainésilver', 'Aithlin'
+                 ]
